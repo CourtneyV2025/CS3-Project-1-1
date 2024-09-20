71c8e37c33d5d614535fbbc7875beb2c74df7173
CourtneyV2025 <courtney.valis2025@gbwl.org>
2024-09-20 14:37:34 +0000

    main.py

--- main.py
+++ main.py
@@ -1,5 +1,55 @@
+# You can initializae important data at the top of the page 
+score = 0
+trivia = { "q": "question", "a": "correct answer", "options": ["choice a", "choice b", "choice c", "choice d", "choice e", "choice f"]},
+
+trivia = [
+    { 
+     "question": "How many presidents were born in Virginia?",
+        "answer": 8,
+        "options": [1, 2, 4, 5, 7, 8, 11]
+     },
+    { "question": "How many presidents were born in New York?",
+        "answer": 5,
+        "options": [1, 2, 4, 5, 6, 10]
+     },
+    { "question": "How many presidents were born in Ohio?",
+        "answer": 7,
+        "options": [1, 2, 4, 5, 7, 8]
+     },
+    { "question": "How many presidents were born in Texas?",
+        "answer": 2,
+        "options": [1, 2, 4, 5, 7, 8]
+     },
+    { "question": "How many presidents were born in Pennsylvania?",
+        "answer": 2,
+        "options": [0, 1, 2, 5, 7, 8]
+     },
+    { "question": "How many presidents were born in Vermont?",
+        "answer": 2,
+        "options": [0, 2, 4, 5, 7, 8, 9]
+     },
+    { "question": "How many presidents were born in Iowa?",
+        "answer": 1,
+        "options": [0, 1, 4, 5, 7, 9]
+     },
+    { "question": "How many presidents were born in Illinois?",
+        "answer": 1,
+        "options": [1, 3, 4, 6, 7, 8]
+     },
+    { "question": "How many presidents were born in Massachussets?",
+        "answer": 4,
+        "options": [1, 2, 4, 5, 7, 9]
+     },
+    { "question": "How many presidents were born in North Carolina?",
+        "answer": 2,
+        "options": [1, 2, 3, 6, 7, 8]
+     }
+]
+
+
+
 def main():
-    print("hello world")
+    print("Let's play 🇺🇸 US Presidents 🇺🇸 Trivia!! ")
 
 
 if __name__ == "__main__":
